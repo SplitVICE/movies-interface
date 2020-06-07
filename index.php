5e6338f93f5da5555a021e4748942a13ab5b1c15
VICE <vice@mail.com>
2020-06-07 15:37:14 -0600

    Fixes
    Grammar mistakes fixed.

--- index.php
+++ index.php
@@ -17,15 +17,15 @@ include './public/php/app.php';
         <h1><?php echo $INI_VARIABLES_ARRAY['INDEX_TITLE'] ?></h1>
     </header>
 
-    <!-- JavaScript will render content page here. -->
+    <!-- JavaScript will render the content of the page here. -->
     <div id="main_content">
 
     </div>
 
     <script>
         try {
-            // This variables are created at app.js file.
-            // This variables will be filled with the backend response of the movies list.
+            // These variables are created at app.js file.
+            // These variables will be filled with the backend response of the movies list.
             movies_names_array = <?php echo json_encode($movies_names_array); ?>;
             movies_folder_path = <?php echo json_encode($movies_folder_path); ?>;
             button_to_reload_movies_list_text = <?php echo json_encode($button_to_reload_movies_list_text) ?>;
@@ -33,6 +33,7 @@ include './public/php/app.php';
             // Main function to render the frontend. Function contained inside app.js .
             main();
         } catch (error) {
+            // If there is a frontend error, this <h1> tag will be added at the header of the page.
             document.getElementsByTagName('header')[0].innerHTML = '<h1>A JS error has occurred.</h1>';
             console.log(error);
         }
